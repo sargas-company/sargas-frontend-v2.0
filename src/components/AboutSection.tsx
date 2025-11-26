25e4fef573afd89c3c24a8c164873ba67403cfc8
Dmytro Sarafaniuk <sarafaniuk.dima@gmail.com>
2025-11-26 17:34:54 +0200

    Animated main section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import {RecentWorkButton} from './RecentWorkButton.tsx'
+import { RecentWorkButton } from "./RecentWorkButton.tsx";
 
 type MarqueeColumnProps = {
     images: string[];
@@ -48,7 +48,6 @@ const MarqueeColumn = ({
     );
 };
 
-
 const leftColumnImages = [
     "https://framerusercontent.com/images/670uUrkwoRnzhCl9b3kEMwUmgE4.jpg?width=1120&height=840",
     "https://framerusercontent.com/images/J4Ox47KYv4g8Lb2C0PXNkjDaA.jpg?width=1120&height=840",
@@ -72,24 +71,31 @@ export const AboutSection = () => {
         <section className="relative isolate flex min-h-screen w-full items-center justify-center">
             <div className="relative w-full max-w-[1440px] border-8 border-white/40 rounded-[36px]">
                 <div className="relative aspect-[9/16] w-full overflow-hidden rounded-[36px] bg-[#181818]">
-                    <div className="relative flex h-full flex-col md:flex-row gap-4 md:gap-5 px-5">
-                        <MarqueeColumn images={leftColumnImages} speed={60} />
-                        <MarqueeColumn
-                            images={rightColumnImages}
-                            reverse
-                            speed={60}
+                    <motion.div
+                        className="relative h-full"
+                        initial={{ opacity: 0, y: 80 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{
+                            delay: 1.6,
+                            duration: 1.4,
+                            ease: "easeInOut",
+                        }}
+                    >
+                        <div className="relative flex h-full flex-col md:flex-row gap-4 md:gap-5 px-5">
+                            <MarqueeColumn images={leftColumnImages} speed={60} />
+                            <MarqueeColumn
+                                images={rightColumnImages}
+                                reverse
+                                speed={60}
+                            />
+                        </div>
 
-                        />
-                    </div>
-
-                    <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 items-center justify-center">
-                        <RecentWorkButton />
-                    </div>
+                        <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 items-center justify-center">
+                            <RecentWorkButton />
+                        </div>
+                    </motion.div>
                 </div>
             </div>
         </section>
     );
 };
-
-
-
