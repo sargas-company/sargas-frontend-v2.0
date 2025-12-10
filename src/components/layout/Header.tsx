2175bd6eccf28c344740042cfa7c8d7c4d5bd9b4
Dmytro Sarafaniuk <sarafaniuk.dima@gmail.com>
2025-12-10 22:17:15 +0200

    Current state

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { motion } from 'framer-motion'
 import { HeaderMenuButton } from './HeaderMenuButton'
 import { HeaderDropdown } from './HeaderDropdown'
